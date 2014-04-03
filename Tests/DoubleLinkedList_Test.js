7c97e34582fd343194501b557bd998f336639dd5
Battistella Stefano <stefano.battistella.92@gmail.com>
2014-04-04 00:58:25 +0200

    Implemented the callback for sort evaluation.

--- Tests/DoubleLinkedList_Test.js
+++ Tests/DoubleLinkedList_Test.js
@@ -40,10 +40,27 @@ test("DoubleLinkedList - Remove at test", function () {
 
 test("DoubleLinkedList - Sort test", function () {
 	var list = new DoubleLinkedList();
-	for(var i = 0; i < 100; i++) {
+	const length = 100;
+	for (var i = 0; i < length; i++)
 		list.pushFront(i);
-	}
+
 	list.sort();
 	deepEqual(list.getItem(0), 0, "Sort");
-	deepEqual(list.getItem(100), 99, "Sort");
+	deepEqual(list.getItem(length - 1), length - 1, "Sort");
+});
+
+test("DoubleLinkedList - Sort with callback test", function () {
+	var list = new DoubleLinkedList();
+	const length = 100;
+
+	for (var i = 0; i < length; i++)
+		list.pushFront({value: i});
+
+	list.sort(
+		function (item) {
+			return item.value;
+		}
+	);
+	deepEqual(list.getItem(0).value, 0, "Sort");
+	deepEqual(list.getItem(length - 1).value, length - 1, "Sort");
 });
