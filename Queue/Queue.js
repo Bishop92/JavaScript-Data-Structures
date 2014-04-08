abf487f37f6ba959dcaa52b3e79adb373270b0e9
Battistella Stefano <stefano.battistella.92@gmail.com>
2014-04-08 09:31:43 +0200

    Added method peek

--- Queue/Queue.js
+++ Queue/Queue.js
@@ -9,24 +9,23 @@
 function Queue() {
 	/**
 	 * The list of the items in the queue.
-	 * @type {Array<Object>}
+	 * @type {Array<*>}
 	 */
 	this.items = [];
 }
 
 /**
  * Add the item at the tail of the queue.
- * @param item The item to add.
+ * @param item {*} The item to add.
  * @return {void}
  */
 Queue.prototype.enqueue = function (item) {
 	this.items.push(item);
 };
 
-//noinspection FunctionWithMultipleReturnPointsJS
 /**
  * Remove the item at the head of the queue.
- * @return {Object|undefined} The item at the head of the queue. It's undefined if the queue is empty.
+ * @return {*} The item at the head of the queue. It's undefined if the queue is empty.
  */
 Queue.prototype.dequeue = function () {
 	if (!this.items.length)
@@ -34,11 +33,10 @@ Queue.prototype.dequeue = function () {
 	return this.items.splice(0, 1)[0]; //remove the first item and return it
 };
 
-//noinspection FunctionWithMultipleReturnPointsJS
 /**
  * Return the item at the position index.
- * @param index The position of the item.
- * @return {Object|undefined} The item at the position. It's undefined if index isn't in the queue bounds.
+ * @param index {number} The position of the item.
+ * @return {*} The item at the position. It's undefined if index isn't in the queue bounds.
  */
 Queue.prototype.getItem = function (index) {
 	if (index < 0 || index > this.items.length - 1)
@@ -47,8 +45,18 @@ Queue.prototype.getItem = function (index) {
 };
 
 /**
+ * Return the first item in the queue. The item is not removed.
+ * @return {*} The first item. It's undefined if the queue is empty.
+ */
+Queue.prototype.peek = function () {
+	if (this.items.length)
+		return this.items[0];
+	return undefined
+};
+
+/**
  * Return the length of the queue.
- * @return {Number} The length of the queue.
+ * @return {number} The length of the queue.
  */
 Queue.prototype.getLength = function () {
 	return this.items.length;
