74c1862740b73a947e5b368cbbd7a73a2056634d
Battistella Stefano <stefano.battistella.92@gmail.com>
2015-02-03 22:20:37 +0100

    Added update of the tree and search of an item by the string

--- Tests/Trie_Test.js
+++ Tests/Trie_Test.js
@@ -7,8 +7,14 @@ test("Trie - Insert test", function () {
     trie.insert("Blue");
     trie.insert("Bleu");
     var strings = [];
-    trie.stringsToArray(strings)
+    trie.stringsToArray(strings);
     deepEqual(strings, ["Bleu","Blue"], "Insert node");
+    trie.insert("Abba", 0);
+    trie.insert("Hello", 0);
+    trie.insert("World", 0);
+    deepEqual(trie.getItem("Abba"), 0, "Insert node");
+    deepEqual(trie.getItem("Hello"), 0, "Insert node");
+    deepEqual(trie.getItem("World"), 0, "Insert node");
 });
 
 test("Trie - Suggest test", function () {
@@ -25,4 +31,22 @@ test("Trie - Suggest test", function () {
     deepEqual(trie.suggest("Ho"), ["Hola"], "Suggest Ho string");
     deepEqual(trie.suggest("A"), [], "Suggest A string");
     deepEqual(trie.suggest("Ready"), [], "Suggest Ready string");
+});
+
+test("Trie - Update Item test", function () {
+    var trie = new Trie();
+    trie.insert("Blue", 0);
+    trie.insert("Boing", 0);
+    trie.insert("Yellow", 0);
+    trie.insert("Hello", 0);
+    trie.insert("He", 0);
+    trie.insert("Hola", 0);
+    var callback = function(item) {
+        return item+1;
+    };
+    var strings = trie.suggest("");
+    for(var i = 0; i < strings.length; ++i) {
+        trie.updateItem(strings[i], callback);
+        deepEqual(trie.getItem(strings[i]), 1, "Value Updated");
+    }
 });
